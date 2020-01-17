64416c78b1a205df1fed9844d842cdff04be8d61
Mikko <mikko.luhtasaari@tuni.fi>
2020-01-17 13:46:31 +0200

    Add prints

--- hello_world.py
+++ hello_world.py
@@ -1,3 +1,8 @@
 print("Hello world!")
+<<<<<<< HEAD
 print("Hello darkness my old friend")
 print("I've come to talk with you again")
+=======
+print("Not another hello world")
+print("No")
+>>>>>>> 94c4fbf (Line change)
